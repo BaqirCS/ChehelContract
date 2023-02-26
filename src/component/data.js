5e676e34495b4a401bc98791deab0e6aaf03a730
baqirEsfandiari1996 <baqiresfandiari74@gmail.com>
2023-02-26 18:30:02 +0330

    chehl soton commit initial

---
+++ src/component/data.js
@@ -0,0 +1,311 @@
+export const extraFood = [
+  {
+    name: 'لقمه خالی',
+    id: 1,
+    price: 0,
+    overal: 160000,
+    check: false,
+    number: 0,
+  },
+  {
+    name: 'چلو گوشت',
+    id: 3,
+    price: 0,
+    overal: 240000,
+    check: false,
+    number: 0,
+  },
+  {
+    name: 'لقمه با برنج',
+    id: 2,
+    price: 0,
+    overal: 180000,
+    check: false,
+    number: 0,
+  },
+  {
+    name: 'برنج خالی',
+    id: 4,
+    price: 0,
+    overal: 80000,
+    check: false,
+    number: 0,
+  },
+
+  {
+    name: 'جوجه خالی',
+    id: 7,
+    price: 0,
+    overal: 160000,
+    check: false,
+    number: 0,
+  },
+
+  {
+    name: 'شوید پلو',
+    id: 5,
+    price: 0,
+    overal: 90000,
+    check: false,
+    number: 0,
+  },
+  {
+    name: 'جوجه با برنج',
+    id: 6,
+    price: 0,
+    overal: 180000,
+    check: false,
+    number: 0,
+  },
+];
+
+export const mainFood = [
+  {
+    name: 'جوجه لقمه گوشت',
+    id: 52,
+    price: 255000,
+  },
+
+  {
+    name: 'جوجه و لقمه',
+    id: 50,
+    price: 195000,
+  },
+
+  {
+    name: 'چلو جوجه',
+    id: 57,
+    price: 195000,
+  },
+  {
+    name: ' زرشک پلو مرغ گوشت ',
+    id: 56,
+    price: 255000,
+  },
+  {
+    name: 'چلو گوشت',
+    id: 55,
+    price: 288000,
+  },
+
+  {
+    name: 'مرغ و لقمه ',
+    id: 54,
+    price: 195000,
+  },
+  {
+    name: 'چلو لقمه',
+    id: 58,
+    price: 195000,
+  },
+  {
+    name: 'زرشک پلو مرغ',
+    id: 51,
+    price: 148000,
+  },
+];
+
+export const designItems = [
+  {
+    name: 'سوپ',
+    id: 210,
+    price: 0,
+    check: false,
+    overal: 10500,
+    type: 'food',
+    number: 0,
+  },
+  {
+    name: 'بستنی',
+    id: 206,
+    price: 0,
+    check: false,
+    overal: 16000,
+    type: 'food',
+    number: 0,
+  },
+
+  {
+    name: 'ژله',
+    id: 214,
+    price: 0,
+    check: false,
+    overal: 8500,
+    type: 'food',
+    number: 0,
+  },
+  {
+    name: 'ماست موسیر',
+    id: 220,
+    price: 0,
+    check: false,
+    overal: 8000,
+    type: 'food',
+    number: 0,
+  },
+
+  {
+    name: 'چای نسکافه',
+    id: 212,
+    price: 0,
+    check: false,
+    overal: 14000,
+    type: 'food',
+    number: 0,
+  },
+
+  {
+    name: 'زیتون',
+    id: 221,
+    price: 0,
+    check: false,
+    overal: 13000,
+    type: 'food',
+    number: 0,
+  },
+  {
+    name: 'چای نبات',
+    id: 215,
+    price: 0,
+    check: false,
+    overal: 9000,
+    type: 'food',
+    number: 0,
+  },
+  {
+    name: 'اتاق عقد',
+    id: 203,
+    price: 1500000,
+    check: false,
+    type: 'simple',
+  },
+  {
+    name: 'رقص نور اکو',
+    id: 201,
+    price: 6000000,
+    check: false,
+    type: 'simple',
+  },
+
+  {
+    name: 'بادکنک ارایی',
+    id: 204,
+    price: 1700000,
+    check: false,
+    type: 'simple',
+  },
+
+  {
+    name: 'نوازنده سنتور',
+    id: 202,
+    price: 2000000,
+    check: false,
+    type: 'simple',
+  },
+
+  {
+    name: 'مه سرد',
+    id: 211,
+    price: 1200000,
+    check: false,
+    type: 'simple',
+  },
+
+  {
+    name: ' سیستم صوت ',
+    id: 208,
+    price: 2000000,
+    check: false,
+    type: 'simple',
+  },
+  {
+    name: 'اتاق شام',
+    id: 209,
+    price: 2000000,
+    check: false,
+    type: 'simple',
+  },
+  {
+    name: ' حباب',
+    id: 478,
+    price: 1800000,
+    check: false,
+    type: 'simple',
+  },
+  {
+    name: ' کالسکه با اسب',
+    id: 479,
+    price: 2000000,
+    check: false,
+    type: 'simple',
+  },
+  {
+    name: 'دف نواز',
+    id: 207,
+    price: 0,
+    overal: 1000000,
+    check: false,
+    type: 'number',
+    number: 0,
+  },
+  {
+    name: 'بال فرشته',
+    id: 205,
+    price: 0,
+    overal: 1200000,
+    check: false,
+    type: 'number',
+    number: 0,
+  },
+];
+
+export const fireWorks = [
+  {
+    name: 'چهار ابشار',
+    id: 400,
+    price: 2000000,
+    check: false,
+  },
+  {
+    name: 'شیش ابشار',
+    id: 401,
+    price: 2500000,
+    check: false,
+  },
+  {
+    name: 'هشت ابشار',
+    id: 402,
+    price: 2800000,
+    check: false,
+  },
+  {
+    name: 'ده ابشار',
+    id: 403,
+    price: 3200000,
+    check: false,
+  },
+  {
+    name: 'دوازده ابشار',
+    id: 404,
+    price: 3500000,
+    check: false,
+  },
+  {
+    name: 'چهل ابشار',
+    id: 405,
+    price: 12000000,
+    check: false,
+  },
+  {
+    name: 'فلکه ای سه شاخه',
+    id: 406,
+    price: 2000000,
+    check: false,
+  },
+  {
+    name: 'فلکه ای شیش شاخه',
+    id: 407,
+    price: 3000000,
+    check: false,
+  },
+];
